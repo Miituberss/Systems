35fab8449011ea16fa05b5732621aca69027429c
Marcos Berrocal <95973582+Miituberss@users.noreply.github.com>
2024-06-26 12:15:21 +0200

    Update HTBLinuxServers.sh

--- Services/HTBLinuxServers.sh
+++ Services/HTBLinuxServers.sh
@@ -1,5 +1,12 @@
-#Servidor Web
+#Lanzar Servidor Web
 ;Python3
 python3 -m http.server #Inicia un server web en el puerto TCP/8000 con el directorio actual
 python3 -m http.server --directory /home/cry0l1t3/target_files #Indica la carpeta que contiene los recursos
-python3 -m http.server -p 443 #Indica el puerto donde hospedar la web 
+python3 -m http.server -p 443 #Indica el puerto donde hospedar la web
+
+;PHP
+php -S 127.0.0.1:8080
+
+;NPM
+npm install -g http-server   # install dependency
+http-server -p 8080
