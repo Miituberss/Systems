21e9677a701092b5d5ea10ad2a531563e63a144d
Marcos Berrocal <95973582+Miituberss@users.noreply.github.com>
2024-06-26 11:42:46 +0200

    Create LinuxServers.sh

---
+++ LinuxServers.sh
@@ -0,0 +1,5 @@
+#Servidor Web
+;Python3
+python3 -m http.server #Inicia un server web en el puerto TCP/8000 con el directorio actual
+python3 -m http.server --directory /home/cry0l1t3/target_files #Indica la carpeta que contiene los recursos
+python3 -m http.server -p 443 #Indica el puerto donde hospedar la web 
